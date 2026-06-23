1e1bc3c4da3f720886bcd51289b8036a1822eba3
Shalehin Ahmed Ornob <saornob2001@gmail.com>
2026-06-24 03:29:16 +0600

    Deploy test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors(
     {
-        origin: [process.env.CLIENT_URL],
+        origin: [process.env.CLIENT_URL, 'https://ghurni.vercel.app'],
         credentials: true
     }
 ))
@@ -36,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("connected!")
